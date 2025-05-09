edc98a96cb0b30efa336be5e2c05f033654f463a
Gabriel Demesio <cfmoneygamer@gmail.com>
2025-05-09 20:59:10 -0300

    creating directories

---
+++ src/main/java/hms/systemhospital/Controller/PatientController.java
@@ -0,0 +1,4 @@
+package hms.systemhospital.Controller;
+
+public class PatientController {
+}
---
+++ src/test/java/hms/systemhospital/SystemHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package hms.systemhospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
